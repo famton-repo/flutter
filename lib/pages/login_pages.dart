df0bf3d5b4ede1a3948c6b1dfdc27d7166290ac3
muthiah-nagalingamdev <muthiah.nagalingam-famteam@outlook.com>
2026-04-22 21:21:55 +0530

    added file

---
+++ lib/pages/login_pages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Login_page extends StatelessWidget {
+  const Login_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
